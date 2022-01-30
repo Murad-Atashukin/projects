41a112355498df9d1237bc15a43b5e448cf20d69
Murad-Atashukin <aslan0799.am@gmail.com>
2022-01-30 21:27:25 +0300

    initial commit

---
+++ test_main_page.py
@@ -0,0 +1,6 @@
+
+def test_guest_should_see_login_link_pass(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    browser.get(link)
+    login_link = browser.find_element_by_css_selector("#login_link")
+    login_link.click()
