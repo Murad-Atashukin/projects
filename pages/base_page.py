d36bd752a74c5f016f2a5a8197f9f9f531374f45
Murad-Atashukin <aslan0799.am@gmail.com>
2022-02-04 19:53:09 +0300

    added files: locators.py and login_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,5 @@
-#from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException
+
 class BasePage:
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
@@ -8,7 +9,7 @@ class BasePage:
     def is_element_present(self, how, what):
         try:
             self.browser.find_element(how, what)
-        except :
+        except (NoSuchElementException):
             return False
         return True
 
